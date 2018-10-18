b84fe285fc7f1ef8e1db24b55ba134ef052843ee
IntrntSrfr <aleksander@tosbakken.com>
2018-10-18 14:44:06 +0200

    doesnt work 123

--- events/ready.go
+++ events/ready.go
@@ -3,9 +3,13 @@ package events
 import (
 	"fmt"
 
+	"test/services"
+
 	"github.com/bwmarrin/discordgo"
 )
 
 func ReadyHandler(s *discordgo.Session, r *discordgo.Ready) {
 	fmt.Println(r.User.Username)
+
+	fmt.Println(services.Commands)
 }
--- services/commandhandler.go
+++ services/commandhandler.go
@@ -6,9 +6,11 @@ import (
 
 //Command is the base for all commands
 type Command struct {
-	name     string
-	aliases  []string
-	function func(s *discordgo.Session, m *discordgo.MessageCreate)
+	name        string
+	aliases     []string
+	description string
+	usage       string
+	function    func(s *discordgo.Session, m *discordgo.MessageCreate)
 }
 
 //Commands is the map for all commands
@@ -16,4 +18,12 @@ type Commands map[string]Command
 
 func init() {
 	commands := make(Commands)
+
+	commands["jeff"] = Command{name: "jeff",
+		aliases:     []string{},
+		description: "does epic",
+		usage:       "command",
+		function: func(s *discordgo.Session, m *discordgo.MessageCreate) {
+			s.ChannelMessageSend(m.ChannelID, "jeff")
+		}}
 }
